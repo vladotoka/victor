de805804586ea43eb9c983dd5793c1ab92227ed4
Vladimir <vladotoka@gmail.com>
2022-11-12 00:02:07 +0200

    stack navigator

---
+++ src/navigation/MainNavigator.js
@@ -0,0 +1,39 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import { createDrawerNavigator } from '@react-navigation/drawer';
+
+import WelcomeScreen from '../screens/WelcomeScreen';
+import Test from '../screens/Test';
+
+const Stack = createNativeStackNavigator();
+// const Drawer = createDrawerNavigator();
+
+function MainStack() {
+	return (
+			<Stack.Navigator>
+				<Stack.Screen
+					name="WelcomeScreen"
+					component={WelcomeScreen}
+					options={{ title: 'Интересно' }}
+				/>
+			</Stack.Navigator>
+	);
+}
+
+
+// function MainDrawer() {
+// 	return (
+// 		<Drawer.Navigator>
+// 			<Drawer.Screen name="NASA api" component={MainStack} />
+// 			<Drawer.Screen name="Test" component={Test} />
+// 		</Drawer.Navigator>
+// 	);
+// }
+
+export default function MainNavigator() {
+	return (
+		<NavigationContainer>
+			<MainStack></MainStack>
+		</NavigationContainer>
+	);
+}
