cabea062c7004018e4bcd110f57dd6015501be35
Vladimir <vladotoka@gmail.com>
2022-11-12 01:28:47 +0200

    babel config

--- src/navigation/MainNavigator.js
+++ src/navigation/MainNavigator.js
@@ -1,12 +1,12 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-// import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 import WelcomeScreen from '../screens/WelcomeScreen';
 import Test from '../screens/Test';
 
 const Stack = createNativeStackNavigator();
-// const Drawer = createDrawerNavigator();
+const Drawer = createDrawerNavigator();
 
 function MainStack() {
 	return (
@@ -21,19 +21,19 @@ function MainStack() {
 }
 
 
-// function MainDrawer() {
-// 	return (
-// 		<Drawer.Navigator>
-// 			<Drawer.Screen name="NASA api" component={MainStack} />
-// 			<Drawer.Screen name="Test" component={Test} />
-// 		</Drawer.Navigator>
-// 	);
-// }
+function MainDrawer() {
+	return (
+		<Drawer.Navigator>
+			<Drawer.Screen name="NASA api" component={MainStack} />
+			<Drawer.Screen name="Test" component={Test} />
+		</Drawer.Navigator>
+	);
+}
 
 export default function MainNavigator() {
 	return (
 		<NavigationContainer>
-			<MainStack></MainStack>
+			<MainDrawer />
 		</NavigationContainer>
 	);
 }
